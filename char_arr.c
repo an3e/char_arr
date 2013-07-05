325f2062340de76bf4f8deab47e99005c2727c00
Andrei Ehrlich <an3e@gmx.de>
2013-07-05 17:17:38 +0200

    dynamic device number allocation implemented

--- char_arr.c
+++ char_arr.c
@@ -3,12 +3,32 @@
 //device name
 char name[] = "char_arr";
 
+//device number
+dev_t devno = 0;
+
 /**************** driver init function ****************/
 static int __init char_init()
 {
-	printk(KERN_INFO "%s: init function\n", name);
+	int ret = -1, major = 0;
 
-	printk(KERN_INFO "%s: init function done.\n", name);
+	printk(KERN_INFO "%s: init: function start\n", name);
+
+	//try to allocate device number for our driver
+	ret = alloc_chrdev_region( &devno,	//output parameter for first assigned number
+					0,	//first of the requested range of minor numbers
+					1,	//number of minor numbers required
+					name);	//the name of associated device or driver
+						//(same name will appear in /proc/devices)
+	if(ret < 0){
+		printk(KERN_INFO "%s: init: major number allocation failed.\n", name);
+		return ret;
+	}
+
+	//print the major number after successfull device number allocation
+	major = MAJOR(devno);
+	printk(KERN_INFO "%s: init: allocated major number: %i.\n", name, major);
+
+	printk(KERN_INFO "%s: init: function done.\n", name);
 
 	return 0;
 }
@@ -16,9 +36,12 @@ static int __init char_init()
 /**************** driver exit function ****************/
 static void __exit char_exit()
 {
-	printk(KERN_INFO "%s: exit function\n", name);
+	printk(KERN_INFO "%s: exit: function start\n", name);
 
-	printk(KERN_INFO "%s: exit function done.\n", name);
+	printk(KERN_INFO "%s: exit: unregistering the device...\n", name);
+	unregister_chrdev_region(devno, 1);
+
+	printk(KERN_INFO "%s: exit: function done.\n", name);
 }
 
 /**************** register init & exit functions ****************/
